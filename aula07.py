4e410f3cd7ef96aeaacddb70de258cccb2b9dbce
RenanTRS <renantrsouza@gmail.com>
2021-03-08 18:22:29 -0400

    Comentários

--- aula07.py
+++ aula07.py
@@ -9,8 +9,8 @@ n2 = int(input('Digite outro número: '))
 s = n1 + n2
 m = n1 * n2
 d = n1 / n2
-di = n1 // n2
-e = n1 ** n2
+di = n1 // n2 #Serve para divisão inteira
+e = n1 ** n2 #Serve para potências
 
 #Saída dos dados
 print('Soma: {}, Produto: {} e a Divisão: {:.1f}'.format(s, m, d), end=' ') #:.1f -> Serve para identificar as casas decimais
