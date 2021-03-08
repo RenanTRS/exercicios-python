454df6af928d74edca733728b0793d5593f9cca1
RenanTRS <renantrsouza@gmail.com>
2021-03-08 18:20:41 -0400

    Aula 07

---
+++ aula07.py
@@ -0,0 +1,19 @@
+#aula 07 - Operadores Aritméticos
+
+#Renan Souza - 08/03/2021
+#Entrada de dados
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+#Manipulação de dados
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+#Saída dos dados
+print('Soma: {}, Produto: {} e a Divisão: {:.1f}'.format(s, m, d), end=' ') #:.1f -> Serve para identificar as casas decimais
+print('Divisão inteira: {} e Exponênciação: {}'.format(di, e))
+
+#Fim
