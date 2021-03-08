20d4d8509a54331a8522c67f2ad645dd461cca30
RenanTRS <renantrsouza@gmail.com>
2021-03-08 14:33:50 -0400

    Aula-06

---
+++ aula06.py
@@ -0,0 +1,13 @@
+#Aula 06 - Tipos primitivos
+#Renan Souza - 08/03/2021
+
+#Entrada de dados
+n1 = int(input("Digite um número:\n")) #variável n1 do tipo int -> inteiro
+n2 = int(input("Digite outro número:\n")) #variável n2 do tipo int -> inteiro
+
+s = n1 + n2 #variável s recebe a soma das variáveis n1 e n2
+
+#Saída de dados
+print("A soma vale: {}".format(s)) #Saída de dados imprimindo a variável s
+
+#Fim
