9d82e284c6750698ca727604bbfb063c930b83f9
RenanTRS <renantrsouza@gmail.com>
2021-03-11 12:05:38 -0400

    ex005

---
+++ ex005.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número inteiro e mostre na 
+#tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um número: '))
+
+print('Antecessor: {} \nSucessor: {}'.format(n - 1, n + 1))
