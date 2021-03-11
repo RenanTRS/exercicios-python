befd5449720522ff81f87d80e714797d9fd3f900
RenanTRS <renantrsouza@gmail.com>
2021-03-11 12:29:58 -0400

    ex008

---
+++ ex008.py
@@ -0,0 +1,8 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
+
+m = float(input('Informe a quantidade de metros: '))
+
+cm = m * 100
+mm = m * 1000
+
+print('Cm: {} \nMm: {}'.format(cm, mm))
