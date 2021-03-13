1c7e456d8222cf5d7c699cc3bcbb6044bc7f2057
RenanTRS <renantrsouza@gmail.com>
2021-03-13 13:38:37 -0400

    aula09

---
+++ aula09.py
@@ -0,0 +1,40 @@
+#Manipulando texto
+
+frase = 'Curso em Vídeo Python'
+
+#print(frase[0:11:2]) #Serve como fatiador de strings, primeiro número indica o começo, o segundo o final, e o terceiro
+#indica quantas casas o sistema deve pular
+
+#print(len(frase)) #Serve para contar a quantidade de caracteres
+
+#print(frase.count('o')) #Serve para mostar quantas vezes o carcatere "o" apareceu na variável
+
+#print(frase.count('o',0,19)) #Serve para mostrar a quantidade em um intervalo específico
+
+#print(frase.find('deo')) #Serve para mostar a posição em que os caracteres se encontram
+
+#print('Curso' in frase) #Serve para mostrar se existe "Curso" na variável "frase"
+
+
+#frase = frase.replace('Python','Android') Serve para trocar determinado item da variável por outro
+#print(frase)
+
+#print(frase.upper()) #Serve para deixar todos os caracteres maiúsculos
+
+#print(frase.lower()) #Deixa os caracteres em minúsculos
+
+#print(frase.capitalize()) #Deixa apenas o primeiro caractere em minúsculo
+
+#print(frase.title()) #Dexia todas os caracteres depois de espaços em maíúsculos
+
+frase2 = '   Aprenda Python   '
+
+#print(frase2.strip()) #Remove os espaços anteriores e posteriores da string
+
+#print(frase2.rstrip()) #Remove os espaços da direita
+
+#print(frase2.lstrip()) #Remove os espaços esquerda
+
+#frase = frase.split() #Divide a lista pelos espaços
+
+#frase = ''.join(frase) #Junta as listas
