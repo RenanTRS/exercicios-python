8f0aad30283f04a5544b2126ff44f26ed6f21c9b
RenanTRS <renantrsouza@gmail.com>
2021-03-15 12:52:29 -0400

    ex026

---
+++ ex026.py
@@ -0,0 +1,21 @@
+#Faça um programa que leia uma frase pelo teclado e mostre: Qantas vezes aparece a letra "A"
+#Em que posição ela aparece a primeira vez
+#Em que posição ela aparece a última vez
+
+#Entrada de dados-----------------------------------------
+frase = str(input('Digite uma frase: ')).strip().lower()
+
+while frase == '':
+    frase = str(input('Digite uma frase: ')).strip().lower()
+
+#Manipulação de dados---------------------------------------
+qtd = frase.count('a') #Contar a quantidade de "a" na variável "frase"
+l = frase.find('a')
+r = frase.rfind('a')
+
+#Saída de dados---------------------------------------------
+print('Quantas vezes apareceram a letra "A": {}'.format(qtd))
+print('Em que posição a letra "A" aparece pela primeira vez: {}'.format(l))
+print('Em que posição a letra "A" aparece pela última vez: {}'.format(r))
+
+#END
