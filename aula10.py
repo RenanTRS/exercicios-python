6a337d1e6358c47e5695385c1c05bccf946b6d26
RenanTRS <renantrsouza@gmail.com>
2021-03-16 17:03:35 -0400

    Validação de nota

--- aula10.py
+++ aula10.py
@@ -1,14 +1,37 @@
-n1 = float(input('Informe a primeira nota: '))
-while n1 > 10:
-    n1 = float(input('Resultado inválido. Informe a primeira nota: '))
+#Validação da primeira nota----------------------------
+nota = str(input('Digite a primeira nota: ')).strip()
+while nota == '' or nota.isalpha() == True:
+    nota = str(input('Informe a primeira nota: ')).strip()
 
-n2 = float(input('Informe a segunda nota: '))
-while n2 > 10:
-    n2 = float(input('Resultado inválido. Informe a segunda nota: '))
+nota1 = float(nota)
 
-media = (n1 + n2)/2
+while nota1 > 10:
+    nota = str(input('Nota inválida. Informe a primeira nota: ')).strip()
+    while nota == '' or nota.isalpha() == True:
+        nota = str(input('Nota inválida. Informe a primeira nota: ')).strip()
 
+    nota1 = float(nota)
+
+#Validação da segunda nota------------------------------------------
+nota = str(input('Digite a segunda nota: ')).strip()
+while nota == '' or nota.isalpha() == True:
+    nota = str(input('Informe a segunda nota: ')).strip()
+    
+nota2 = float(nota)
+
+while nota2 > 10:
+    nota = str(input('Nota inválida. Informe a segunda nota: ')).strip()
+    while nota == '' or nota.isalpha() == True:
+        nota = str(input('Nota inválida. Informe a segunda nota: ')).strip()
+
+    nota2 = float(nota)
+
+media = (nota1 + nota2)/2
+
+#Saída------------------------
 if media >= 7:
     print('APROVADO! Sua média foi: {:.1f}'.format(media))
 else:
     print('REPROVADO! Sua média foi {:.1f}'.format(media))
+
+#END
