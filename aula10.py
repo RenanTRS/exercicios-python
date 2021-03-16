3d175da20b9f8207a37650ba887c142731ce85fb
RenanTRS <renantrsouza@gmail.com>
2021-03-16 14:19:51 -0400

    uso do while

--- aula10.py
+++ aula10.py
@@ -1,5 +1,10 @@
 n1 = float(input('Informe a primeira nota: '))
+while n1 > 10:
+    n1 = float(input('Resultado inválido. Informe a primeira nota: '))
+
 n2 = float(input('Informe a segunda nota: '))
+while n2 > 10:
+    n2 = float(input('Resultado inválido. Informe a segunda nota: '))
 
 media = (n1 + n2)/2
 
