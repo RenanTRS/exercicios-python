79ae131122a313e503668202d38ebc32feeb035f
RenanTRS <renantrsouza@gmail.com>
2021-03-20 10:17:23 -0400

    ex041

---
+++ ex041.py
@@ -0,0 +1,18 @@
+#A confederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e mostre
+#sua categoria, de acordo com a idade: Até 9 anos mirim. Até 14 anos infantil. Até 19 anos junior. Até 25 
+#anos sênior. acima master
+
+idade = int(input('Idade do atleta: '))
+
+if idade <= 9:
+    print('{} anos. Categoria mirim.'.format(idade))
+elif idade <= 14:
+    print('{} anos. Categoria infantil.'.format(idade))
+elif idade <= 19:
+    print('{} anos. Categoria junior'.format(idade))
+elif idade <= 25:
+    print('{} anos. Categoria sênior.'.format(idade))
+else:
+    print('{} anos. Categoria master.'.format(idade))
+
+#END
