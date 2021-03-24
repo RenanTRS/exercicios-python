cbd30495019b34ebb9eedb1f25ec1cdb105fb233
RenanTRS <renantrsouza@gmail.com>
2021-03-24 15:35:23 -0400

    Exemplo com string

--- aula14.py
+++ aula14.py
@@ -1,5 +1,11 @@
+'''
 c = 0
 while c < 10:
     print(c)
     c += 1
 print('Fim')
+'''
+r = 's'
+while r == 's':
+    n = int(input('Digite um número: '))
+    r = str(input('Quer continuar [S/N]')).strip().lower()
