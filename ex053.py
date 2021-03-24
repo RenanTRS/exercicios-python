122d24d570cb6e5c2ea6d9180a1b5e8dbb8b8598
RenanTRS <renantrsouza@gmail.com>
2021-03-24 12:40:13 -0400

    feito com for

--- ex053.py
+++ ex053.py
@@ -1,1 +1,16 @@
 #Crie um programa que leia uma frase qualquer e diga se é um palíndromo, desconsidenrado os espaços
+
+frase = str(input('Digite um nome: ')).lower().split()
+frase = ''.join(frase)
+qtd = len(frase)-1
+frase2 = ''
+
+for c in range(qtd, -1, -1):
+    frase2 += frase[c]
+
+if frase2 == frase:
+    print('{} é um palíndromo.'.format(frase2))
+else:
+    print('{} não é um palíndromo.'.format(frase))
+    
+
