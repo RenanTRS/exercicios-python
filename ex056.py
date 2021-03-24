d8cdbc1507d65e31ef172c2ab7155a097e0c621c
RenanTRS <renantrsouza@gmail.com>
2021-03-24 13:35:34 -0400

    if feito

--- ex056.py
+++ ex056.py
@@ -3,9 +3,27 @@
 #Qual é o nome do homem mais velho. 
 #Quantas mulheres têm menos de 20 anos.
 
+maior = mediaIdade = qtdM = 0
+
 for c in range(0, 4):
     print('----{}ºPESSOA----'.format(c+1))
     nome = str(input('Nome: ')).strip().title()
     idade = int(input('Idade: '))
     sexo = str(input('Sexo: ')).strip().title()[0] #Recorte da primeira letra.
+    mediaIdade += idade
+
+    if sexo == 'M' and c == 0:
+        maior = idade
+        nomeVelho = nome
+    elif sexo == 'M' and idade > maior:
+        maior = idade
+        nomeVelho = nome
     
+    if sexo == 'F' and idade < 20:
+        qtdM += 1
+
+mediaIdade = mediaIdade /4
+
+print('Média de idade do grupo: {}'.format(mediaIdade))
+print('O homem mais velho é o {}, ele tem {} anos'.format(nomeVelho, maior))
+print('{} mulheres têm mais de 20 anos'.format(qtdM))
