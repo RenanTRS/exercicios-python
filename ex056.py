9e32f9fe46077c99f278057d93f5a7785f3af7d3
RenanTRS <renantrsouza@gmail.com>
2021-03-24 13:23:44 -0400

    for feito

--- ex056.py
+++ ex056.py
@@ -3,3 +3,9 @@
 #Qual é o nome do homem mais velho. 
 #Quantas mulheres têm menos de 20 anos.
 
+for c in range(0, 4):
+    print('----{}ºPESSOA----'.format(c+1))
+    nome = str(input('Nome: ')).strip().title()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: ')).strip().title()[0] #Recorte da primeira letra.
+    
