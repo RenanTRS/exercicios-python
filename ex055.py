462333ad930ebb09228b76f234f6134c244cf7c0
RenanTRS <renantrsouza@gmail.com>
2021-03-24 13:04:18 -0400

    Estrutura feita

--- ex055.py
+++ ex055.py
@@ -1,2 +1,6 @@
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
+maior = menor = 0
+
+for p in range(0, 5):
+    peso = float(input('Informe o peso da {}º pessoa: '.format(p+1)))
