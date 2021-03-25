d98a4f6da7fb7c27edece33f640735c8e3a56dd9
RenanTRS <renantrsouza@gmail.com>
2021-03-25 16:49:22 -0400

    uso do while

--- ex057.py
+++ ex057.py
@@ -1,3 +1,7 @@
 #Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente
 #até ter um valor correto.
 
+sexo = str(input('Digite o sexo: [M/F]: ')).strip().upper()[0]
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('\033[1;31mValor inválido\033[m Digite o sexo: [M/F]: ')).strip().upper()[0]
+print('Sexo {} cadastrado com sucesso.'.format(sexo))
