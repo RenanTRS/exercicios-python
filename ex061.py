7fefc171602ff54bc9d6cd69fc81e5b08940369c
RenanTRS <renantrsouza@gmail.com>
2021-03-26 14:24:23 -0400

    Falta melhorar a lógica

--- ex061.py
+++ ex061.py
@@ -1,3 +1,16 @@
 #Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura
 #while.
 
+primeiroTermo = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razao: '))
+cont = 9
+
+print('{} -> '.format(primeiroTermo), end='')
+while cont >= 1:
+    primeiroTermo += razao
+    if cont == 1:
+        print('{}'.format(primeiroTermo))
+        cont -= 1
+    else:
+        print('{} -> '.format(primeiroTermo), end='')
+        cont -= 1
