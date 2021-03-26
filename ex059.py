83c881a1d4c114e153bc02f2622fd1d0391a44e5
RenanTRS <renantrsouza@gmail.com>
2021-03-25 21:59:51 -0400

    Organizar algoritmo

--- ex059.py
+++ ex059.py
@@ -1,29 +1,36 @@
 #Crie um programa que leia dois valores e mostre um menu na tela: [ 1 ] somar [ 2 ] multiplicar [ 3 ] maior [ 4 ] novos números
 #[ 5 ] sair do programa Seu programa deverá realizar a operação solicitada em cada caso.
-
-flag = True
+from time import sleep
 
 num1 = int(input('Digite o primeiro valor: '))
 num2 = int(input('Digite o segundo valor: '))
+flag = True
+
 while flag == True:    
     print('=-='*20)
     escolha = int(input('[1] - Somar \n[2] - Multiplicar \n[3] - Maior \n[4] - Novos números \n[5] - Sair do programa\n'))
 
-    if escolha == 1:
+    if escolha == 1:#1----------------------------------------------------------
         print('Soma: {}'.format(num1 + num2))
-    elif escolha == 2:
+
+    elif escolha == 2:#2---------------------------------------------------------
         print('Multiplicação: {}'.format(num1 * num2))
-    elif escolha == 3:
+
+    elif escolha == 3: #3---------------------------------------------------------
         if num1 > num2:
             print('{} é o número maior.'.format(num1))
         elif num1 < num2:
             print('{} é o número maior.'.format(num2))
         else:
             print('Não existe número maior, os valores são iguais.')
-    elif escolha == 4:
+
+    elif escolha == 4: #4---------------------------------------------------------
         num1 = int(input('Digite um novo número: '))
         num2 = int(input('Digite um novo número: '))
-    elif escolha ==5:
-        flag = False
 
+    elif escolha ==5:#5------------------------------------------------------------
+        flag = False
+        print('Saíndo....')
+        sleep(1)
+#Fim do laco-----------------------------------------------------------------------------------
 print('Fim')
