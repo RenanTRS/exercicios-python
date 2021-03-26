fbacd7e9133b04263a29d834060ee8a817d60c24
RenanTRS <renantrsouza@gmail.com>
2021-03-25 21:55:24 -0400

    Lógica concluída

--- ex059.py
+++ ex059.py
@@ -1,3 +1,29 @@
-#Exercício Python 059: Crie um programa que leia dois valores e mostre um menu na tela: [ 1 ] somar [ 2 ] multiplicar [ 3 ] maior 
-#[ 4 ] novos números [ 5 ] sair do programa Seu programa deverá realizar a operação solicitada em cada caso.
+#Crie um programa que leia dois valores e mostre um menu na tela: [ 1 ] somar [ 2 ] multiplicar [ 3 ] maior [ 4 ] novos números
+#[ 5 ] sair do programa Seu programa deverá realizar a operação solicitada em cada caso.
 
+flag = True
+
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+while flag == True:    
+    print('=-='*20)
+    escolha = int(input('[1] - Somar \n[2] - Multiplicar \n[3] - Maior \n[4] - Novos números \n[5] - Sair do programa\n'))
+
+    if escolha == 1:
+        print('Soma: {}'.format(num1 + num2))
+    elif escolha == 2:
+        print('Multiplicação: {}'.format(num1 * num2))
+    elif escolha == 3:
+        if num1 > num2:
+            print('{} é o número maior.'.format(num1))
+        elif num1 < num2:
+            print('{} é o número maior.'.format(num2))
+        else:
+            print('Não existe número maior, os valores são iguais.')
+    elif escolha == 4:
+        num1 = int(input('Digite um novo número: '))
+        num2 = int(input('Digite um novo número: '))
+    elif escolha ==5:
+        flag = False
+
+print('Fim')
