f974ba365257dd7752cb3205950e2a6f2a791099
RenanTRS <renantrsouza@gmail.com>
2021-03-27 14:08:02 -0400

    Concluído

--- ex070.py
+++ ex070.py
@@ -4,3 +4,26 @@
 #B) quantos produtos custam mais de R$1000.
 #C) qual é o nome do produto mais barato. 
 
+soma = maisMil = cont = 0
+while True:
+    produto = str(input('Nome do produto: ')).strip().title()
+    valor = float(input('Valor do produto: '))
+    cont += 1
+    soma += valor
+    if valor > 1000:
+        maisMil += 1
+    if cont == 1:
+        barato = produto
+        maisBarato = valor
+    else:
+        if maisBarato > valor:
+            maisBarato = valor
+            barato = produto
+    
+    flag = str(input('Deseja continuar [S/N]: ')).strip().lower()[0]
+    if flag == 'n':
+        break
+print('=='*20)
+print(f'Total gasto R${soma:.2f}')
+print(f'{maisMil} custam mais que R$1000,00')
+print(f'{barato} é o produto mais barato.')
