090280f3c8c699ad55b3c5fd2ecfaffd17657d15
RenanTRS <renantrsouza@gmail.com>
2021-03-26 21:08:47 -0400

    Concluído

--- ex063.py
+++ ex063.py
@@ -1,2 +1,16 @@
 #Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
 
+qtd = int(input('Digite a quantidade de termos: '))
+cont = 3
+t1 = 0
+t2 = 1
+
+print('{} -> {} -> '.format(t1, t2), end='')
+while cont <= qtd:
+    t3 = t1 + t2
+    print('{} -> '.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+
+print('Fim')
