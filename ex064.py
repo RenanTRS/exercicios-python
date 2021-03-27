32b82e7325c520547ae9ec050e14b1a9c6907f32
RenanTRS <renantrsouza@gmail.com>
2021-03-26 21:17:31 -0400

    Concluído

--- ex064.py
+++ ex064.py
@@ -2,3 +2,13 @@
 #é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
 
 
+soma = num = cont = 0
+while num != 999:
+    num = int(input('Digite um número: '))
+    if num == 999:
+        flag = 999
+    else:
+        soma += num
+        cont += 1
+
+print('Você digitou {} números, e a soma entre eles foi {}'.format(cont, soma))
