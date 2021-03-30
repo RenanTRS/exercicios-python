185c1ba1b3724ce21b1e6705f1468354c1a737bc
RenanTRS <renantrsouza@gmail.com>
2021-03-29 21:47:00 -0400

    Concluído

--- ex071.py
+++ ex071.py
@@ -2,3 +2,24 @@
 #(número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. #OBS: considere que o caixa possui 
 #cédulas de R$50, R$20, R$10 e R$1.
 
+valor = int(input('Digite o valor que você quer sacar R$'))
+total = valor
+cedulas = 50
+totalCedulas = 0
+
+while True:
+    if total >= cedulas:
+        total -= cedulas
+        totalCedulas += 1
+    else:
+        if totalCedulas > 0:
+            print(f'Foram usadas {totalCedulas} notas de R${cedulas}')
+        if cedulas == 50:
+            cedulas = 20
+        elif cedulas == 20:
+            cedulas = 10
+        elif cedulas == 10:
+            cedulas = 1
+        totalCedulas = 0
+        if total == 0:
+            break
