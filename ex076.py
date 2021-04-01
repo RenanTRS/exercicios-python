3800f75b69f99d85898a8fea69f8bd32db86707c
RenanTRS <renantrsouza@gmail.com>
2021-04-01 14:46:25 -0400

    Concluído

--- ex076.py
+++ ex076.py
@@ -1,7 +1,7 @@
 #Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final, mostre uma 
 #listagem de preços, organizando os dados em forma tabular.
 
-produtos = ('Cola', '1.50', 'Lápis', '3.00', 'Caderno', '12.00')
+produtos = ('Cola', '1.50', 'Lápis', '3.00', 'Caderno', '12.00', 'Mochila', '55.00')
 
 for c in range(0, len(produtos), 2):
-    print(produtos[c], produtos[c+1])
+    print(f'{produtos[c]:.<30}  R${produtos[c+1]:>8}')
