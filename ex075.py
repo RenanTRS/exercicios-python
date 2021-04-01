5ca18b63d53099a0eb826c45e80b742b8c65ba02
RenanTRS <renantrsouza@gmail.com>
2021-04-01 14:32:47 -0400

    Concluído

--- ex075.py
+++ ex075.py
@@ -11,6 +11,12 @@ n4 = int(input('Digite um último número: '))
 numeros = (n1, n2, n3, n4)
 print(f'O número 9 apareceu: {numeros.count(9)} vezes')
 if numeros.count(3) > 0:
-    print(f'O número 3 na posição: {numeros.index(3)}')
+    print(f'O número 3 na posição: {numeros.index(3)+1}')
 else:
     print(f'Não possui valor 3')
+print('Valores pares digitados: ', end='')
+for c in numeros:
+    if c % 2 == 0:
+        print(c, end=' ')
+
+print('')
