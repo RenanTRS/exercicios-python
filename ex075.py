bdc82ee05e4ce072fc5326f1b49c63f666b96703
RenanTRS <renantrsouza@gmail.com>
2021-04-01 14:20:54 -0400

    condição if para saber se valor 3 foi inserido

--- ex075.py
+++ ex075.py
@@ -9,4 +9,8 @@ n3 = int(input('Digite mais um número: '))
 n4 = int(input('Digite um último número: '))
 
 numeros = (n1, n2, n3, n4)
-print(numeros)
+print(f'O número 9 apareceu: {numeros.count(9)} vezes')
+if numeros.count(3) > 0:
+    print(f'O número 3 na posição: {numeros.index(3)}')
+else:
+    print(f'Não possui valor 3')
