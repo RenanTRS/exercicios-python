f6167039487530de98b9eade29ff9bc9e2405ba7
RenanTRS <renantrsouza@gmail.com>
2021-04-01 07:13:34 -0400

    Concluído

--- ex073.py
+++ ex073.py
@@ -5,3 +5,8 @@
 #c) Times em ordem alfabética. 
 #d) Em que posição está o time da Chapecoense.
 
+timesBrasileirao = ('Corinthians', 'Flamento', 'São Paulo', 'Palmeiras', 'Bota Fogo', 'Santos', 'Grêmio', 'Chapecoense', 'Ponte Preta', 'Atlético', 'Inter', 'Bahia', 'Fluminense', 'Cruzeiro', 'Vasco', 'Ceará', 'Vitória', 'Paraná', 'Atlético-Mg', 'Atletico-Go')
+print(timesBrasileirao[:5])
+print(timesBrasileirao[len(timesBrasileirao)-4:])
+print(sorted(timesBrasileirao))
+print(timesBrasileirao.index('Chapecoense'))
