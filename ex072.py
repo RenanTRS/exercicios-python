10ffcef4faf6651f2ea922c79cf633085693a025
RenanTRS <renantrsouza@gmail.com>
2021-04-01 06:47:18 -0400

    Concluído

--- ex072.py
+++ ex072.py
@@ -1,3 +1,9 @@
-#Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá 
+#Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá 
 #ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numeroExtenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+numero = int(input('Digite um número entre 0 e 20: '))
+while numero < 0 or numero > 20:
+    numero = int(input('Digite um número entre 0 e 20: '))
+print(numeroExtenso[numero].title())
 
