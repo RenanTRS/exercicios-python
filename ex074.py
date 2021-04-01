0ba9297082954d5605c4b02f27d2c9c8b8d22c64
RenanTRS <renantrsouza@gmail.com>
2021-04-01 13:58:13 -0400

    Testando possibilidades

--- ex074.py
+++ ex074.py
@@ -1,3 +1,8 @@
 #Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados
 #e também indique o menor e o maior valor que estão na tupla.
 
+from random import randint
+
+numeros = (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
+
+print(numeros)
