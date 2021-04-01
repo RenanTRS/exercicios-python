c456d40d70367dc041da729b9c92492320ac4706
RenanTRS <renantrsouza@gmail.com>
2021-04-01 14:13:42 -0400

    Testando possibilidades

--- ex075.py
+++ ex075.py
@@ -3,3 +3,10 @@
 #B) Em que posição foi digitado o primeiro valor 3.
 #C) Quais foram os números pares.
 
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite mais um número: '))
+n4 = int(input('Digite um último número: '))
+
+numeros = (n1, n2, n3, n4)
+print(numeros)
