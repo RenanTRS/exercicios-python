10be10edc0dee41e530d77947264e46b3c8f5853
RenanTRS <renantrsouza@gmail.com>
2021-04-01 15:01:53 -0400

    Concluído

--- ex077.py
+++ ex077.py
@@ -1,3 +1,11 @@
 #Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, 
 #quais são as suas vogais.
 
+palavras = ('teste', 'python', 'rex', 'mayuko')
+vogais = ('a', 'e', 'i', 'o', 'u')
+for c in palavras:
+    print(f'{c.upper()} ', end = ' ')
+    for y in c:
+        if y in vogais:
+            print(y, end=' ')
+    print('')
