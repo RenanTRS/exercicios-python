49f57da10450dbe4a43a7ccc2b91ff000c757b67
RenanTRS <renantrsouza@gmail.com>
2021-04-06 14:15:07 -0400

    Finished

--- ex079.py
+++ ex079.py
@@ -1,3 +1,15 @@
 #Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá 
 #dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente. 
-
+number = []
+value = 0
+while True:
+    value = int(input('Enter a number: '))
+    if value not in number:
+        number.append(value)
+    else:
+        print(f'The number {value} already exists')
+    opc = str(input('Do you wish to continue [Y/N]: ')).strip().lower()
+    if 'n' in opc:
+        break
+number.sort()
+print(number)
