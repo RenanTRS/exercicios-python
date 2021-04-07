6f8433df1d44ef2d64c7e3a9aadfb24a26043fd1
RenanTRS <renantrsouza@gmail.com>
2021-04-07 11:07:13 -0400

    Completed, included if and reverse

--- ex081.py
+++ ex081.py
@@ -3,11 +3,22 @@
 #B) A lista de valores, ordenada de forma decrescente.
 #C) Se o valor 5 foi digitado e está ou não na lista.
 
+
+#Variables
 number = []
+
+#list logic
 while True:
     number.append(int(input('Enter a number: ')))
     opc = input('Do you want to continue [Y/N]: ').strip().lower()
     if 'n' in opc:
         break
 
-print(number)
+#code exit
+print(f'Numbers entered {len(number)}')
+number.sort(reverse=True)
+print(f'Decending list: {number}')
+if 5 in number:
+    print('The value 5 was entered.')
+else:
+    print('The value 5 was not entered.')
