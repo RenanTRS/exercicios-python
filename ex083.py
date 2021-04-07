c7640d80ae8d32d1e058cfed6976c6692c5f13dd
RenanTRS <renantrsouza@gmail.com>
2021-04-07 11:42:56 -0400

    Complete, included if

--- ex083.py
+++ ex083.py
@@ -1,3 +1,11 @@
-#: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão 
+#Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão 
 #passada está com os parênteses abertos e fechados na ordem correta.
 
+expression = input('Enter a expression: ')
+
+ope = expression.count('(')
+closed = expression.count(')')
+if ope == closed:
+    print('Valid expression')
+else:
+    print('Invalid expression')
