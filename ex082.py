61db71ff0845f7e96d2cf71b4031adc817a05092
RenanTRS <renantrsouza@gmail.com>
2021-04-07 11:20:44 -0400

    While logic completed

--- ex082.py
+++ ex082.py
@@ -1,3 +1,16 @@
 #Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os
 #valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 
+#Variables
+pair = []
+odd = []
+values = []
+
+while True:
+    number = int(input('Enter a number: '))
+    values.append(number)
+    opc = input('Do you want to continue [Y/N]: ').strip().lower()
+    if 'n' in opc:
+        break
+
+print(values)
