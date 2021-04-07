7eae261bee7722313b05493431d18d82279681de
RenanTRS <renantrsouza@gmail.com>
2021-04-07 11:27:39 -0400

    Completed, included if and code exit

--- ex082.py
+++ ex082.py
@@ -6,11 +6,19 @@ pair = []
 odd = []
 values = []
 
+#list logic
 while True:
     number = int(input('Enter a number: '))
     values.append(number)
+    if number % 2 == 0:
+        pair.append(number)
+    else:
+        odd.append(number)
     opc = input('Do you want to continue [Y/N]: ').strip().lower()
     if 'n' in opc:
         break
 
-print(values)
+#code exit
+print(f'Full list:{values}')
+print(f'Full pairs list:{pair}')
+print(f'Full odd list: {odd}')
