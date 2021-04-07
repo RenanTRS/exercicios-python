4a04ec25593e140b0a4482cef615dde6e1d56e8a
RenanTRS <renantrsouza@gmail.com>
2021-04-07 10:49:31 -0400

    While logic completed

--- ex081.py
+++ ex081.py
@@ -3,3 +3,8 @@
 #B) A lista de valores, ordenada de forma decrescente.
 #C) Se o valor 5 foi digitado e está ou não na lista.
 
+while True:
+    number = int(input('Enter a number: '))
+    opc = input('Do you want to continue [Y/N]: ').strip().lower()
+    if 'n' in opc:
+        break
