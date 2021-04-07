0fa1afb24dc73d657fdf469e63582e0fdac2ff4c
RenanTRS <renantrsouza@gmail.com>
2021-04-06 23:29:34 -0400

    for if else

--- ex080.py
+++ ex080.py
@@ -1,3 +1,13 @@
 #Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção
 #(sem usar o sort()). No final, mostre a lista ordenada na tela.
 
+numberList = []
+cont = bigger = smaller = value = 0
+for c in range(0, 5):
+    value = int(input('Enter a number: '))
+    if cont == 0:
+        numberList.append(value)
+    else:
+        for y in numberList:
+            
+    cont
