37c52191ca5e7a374a3298d3a450f8fa11bf9167
RenanTRS <renantrsouza@gmail.com>
2021-04-10 13:32:36 -0400

    Improvements

--- ex086.py
+++ ex086.py
@@ -1,13 +1,16 @@
 #Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na 
 #tela, com a formatação correta.
 
+#variable
 blocks = [[0, 0, 0], [0, 0, 0], [0, 0, 0,]]
 
+#for logic
 for c in range(0, 3):
     for y in range(0, 3):
         blocks[c][y] = int(input(f'Enter a number for [{c}, {y}]: '))
-        
+
+#for logic Exit        
 for x in blocks:
     for z in x:
         print(f'[ {z} ]', end='')
-    print('')
+    print('')#line break
