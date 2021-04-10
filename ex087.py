996cf6d181e13d5cacbb4e713386ae964b3d1d11
RenanTRS <renantrsouza@gmail.com>
2021-04-10 13:57:59 -0400

    Sum of third column

--- ex087.py
+++ ex087.py
@@ -3,3 +3,22 @@
 #B) A soma dos valores da terceira coluna.
 #C) O maior valor da segunda linha.
 
+#variables
+blocks = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+pair = sColumn = 0
+
+#for logic
+for l in range(0, 3):
+    for c in range(0, 3):
+        blocks[l][c] =  int(input(f'Enter a number for [{l}, {c}]: '))
+        if blocks[l][c] % 2 == 0:
+            pair += blocks[l][c]
+        if c == 2:
+            #print('3')
+            sColumn += blocks[l][c] #sum of third column
+
+
+print(blocks)
+print(pair)
+print(sColumn)
+
