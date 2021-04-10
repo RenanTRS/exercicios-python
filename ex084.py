d2482901aebc603d05efef582cfa03fdd3795259
RenanTRS <renantrsouza@gmail.com>
2021-04-10 10:33:10 -0400

    Completed logic

--- ex084.py
+++ ex084.py
@@ -3,7 +3,20 @@
 #B) Uma listagem com as pessoas mais pesadas.
 #C) Uma listagem com as pessoas mais leves.
 
+#variables
+peopleList = []
+person = []
+weight = []
+bigger = smaller = 0
+
+#while logic
 while True:
+    person.append(str(input('Name: ')).strip().title())
+    person.append(float(input('Weight: ')))
+    peopleList.append(person[:])
+    person.clear()
     opc = str(input('Do you want to continue [Y/N]: ')).strip().lower()
     if 'n' in opc:
         break
+
+print(peopleList)
