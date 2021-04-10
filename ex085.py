a30daf335d133223aaf15e801450a2bd1e5f46f1
RenanTRS <renantrsouza@gmail.com>
2021-04-10 12:25:23 -0400

    Improvements

--- ex085.py
+++ ex085.py
@@ -1,20 +1,20 @@
 #Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os 
 #valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
-numberList = []
-pair = []
-odd = []
+#variable
+numberList = [[], []]
 
+#for logic
 for c in range(1, 8):
     number = int(input(f'Enter the {c}º number: '))
-    numberList.append(number)
     if number % 2 == 0:
-        pair.append(number)
+        numberList[0].append(number)
     else:
-        odd.append(number)
+        numberList[1].append(number)
 
-pair.sort()
-odd.sort()
-#print(numberList)
-print(f'Pair numbers: {pair}')
-print(f'Odd numbers: {odd}')
+numberList[0].sort()
+numberList[1].sort()
+
+#code exit
+print(f'Pair numbers: {numberList[0]}')
+print(f'Odd numbers: {numberList[1]}')
