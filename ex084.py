b4ad7730ee2700ffd6b371d0b5570b3d550957e8
RenanTRS <renantrsouza@gmail.com>
2021-04-10 10:21:43 -0400

    while logic

--- ex084.py
+++ ex084.py
@@ -3,3 +3,7 @@
 #B) Uma listagem com as pessoas mais pesadas.
 #C) Uma listagem com as pessoas mais leves.
 
+while True:
+    opc = str(input('Do you want to continue [Y/N]: ')).strip().lower()
+    if 'n' in opc:
+        break
