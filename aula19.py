209dd76b7acd9c7179c7dc5258de34114283ad64
RenanTRS <renantrsouza@gmail.com>
2021-04-15 13:46:47 -0400

    Teste de print formatado

--- aula19.py
+++ aula19.py
@@ -2,3 +2,4 @@ pessoas = {'nome': 'Renan', 'sexo': 'M', 'idade': 24}#Declarando dicionário
 print(pessoas)
 print(pessoas['nome'])
 print(pessoas['idade'])
+print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')
