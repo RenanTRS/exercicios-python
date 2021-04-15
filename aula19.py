fd2f1bb7cc14063eda2fb1186332c92f86edfa4b
RenanTRS <renantrsouza@gmail.com>
2021-04-15 13:54:29 -0400

    forma de usar o for para pegar keys e values

--- aula19.py
+++ aula19.py
@@ -1,8 +1,11 @@
 pessoas = {'nome': 'Renan', 'sexo': 'M', 'idade': 24}#Declarando dicionário
-print(pessoas)
-print(pessoas['nome'])
-print(pessoas['idade'])
-print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')#aspas duplas porque as simples já estavam em uso
-print(pessoas.keys()) #mostra o nome das posições.
-print(pessoas.values()) #mostra apenas os valores.
-print(pessoas.items()) #mostra as posições e seus respectivos valores.
+#print(pessoas)
+#print(pessoas['nome'])
+#print(pessoas['idade'])
+#print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')#aspas duplas porque as simples já estavam em uso
+#print(pessoas.keys()) #mostra o nome das posições.
+#print(pessoas.values()) #mostra apenas os valores.
+#print(pessoas.items()) #mostra as posições e seus respectivos valores.
+for k, v in pessoas.items(): #Forma de usar o for para pegar tanto às chaves como os valores
+    print(k, v)
+    
