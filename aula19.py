fdf30d6d992fdc52001956fd8f7f4c0c01408028
RenanTRS <renantrsouza@gmail.com>
2021-04-15 14:14:01 -0400

    Utilizando for com imputs nos dicionários e fazendo cópia com .copy()

--- aula19.py
+++ aula19.py
@@ -17,6 +17,7 @@ pessoas['peso'] = 62.0 #é possível adicionar novas keys
 for k, v in pessoas.items():
     print(k, v)
 '''
+'''
 brasil = []
 estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
 estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
@@ -25,3 +26,11 @@ brasil.append(estado1)
 brasil.append(estado2)
 print(brasil)
 print(brasil[0]['uf'])
+'''
+estado = dict() #Outra forma de iniciar um dicionário.
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['silga'] = str(input('Siglas do Estado: '))
+    brasil.append(estado.copy()) #serve para fazer cópias, igual ao [:] das listas.
+print(brasil)
