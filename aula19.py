69377eabfc3f0fc706104d995047141993cb8ec0
RenanTRS <renantrsouza@gmail.com>
2021-04-15 13:49:00 -0400

    .keys()

--- aula19.py
+++ aula19.py
@@ -2,4 +2,6 @@ pessoas = {'nome': 'Renan', 'sexo': 'M', 'idade': 24}#Declarando dicionário
 print(pessoas)
 print(pessoas['nome'])
 print(pessoas['idade'])
-print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')
+print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')#aspas duplas porque as simples já estavam em uso
+print(pessoas.keys()) #mostra o nome das posições.
+
