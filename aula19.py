fee33837facf7ee1e68bb90b6febc6a01b76fb14
RenanTRS <renantrsouza@gmail.com>
2021-04-15 13:50:10 -0400

    .values()

--- aula19.py
+++ aula19.py
@@ -4,4 +4,4 @@ print(pessoas['nome'])
 print(pessoas['idade'])
 print(f'{pessoas["nome"]} tem {pessoas["idade"]} anos')#aspas duplas porque as simples já estavam em uso
 print(pessoas.keys()) #mostra o nome das posições.
-
+print(pessoas.values()) #mostra apenas os valores.
