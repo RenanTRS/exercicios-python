780d2ebb5460bc5dde2d0571c9a925a0ddeacdd5
RenanTRS <renantrsouza@gmail.com>
2021-04-16 12:05:02 -0400

    Library and For included

--- ex091.py
+++ ex091.py
@@ -1,3 +1,9 @@
-#Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. 
+#Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python.
 #No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
+#library
+from random import randint
+
+#for logic
+for c in range(0, 4):
+    print(randint(1, 6))
