843b07093d4b6a1f2fda932b26db58a727937bb0
RenanTRS <renantrsouza@gmail.com>
2021-04-16 13:34:40 -0400

    sorted() and itemgetter included

--- ex091.py
+++ ex091.py
@@ -3,6 +3,7 @@
 
 #library
 from random import randint
+from operator import itemgetter
 
 #variable
 rank = {}
@@ -10,5 +11,6 @@ game = {'player1': randint(1, 6), 'player2': randint(1, 6), 'player3': randint(1
 #for logic
 for k, v in game.items():
     print(f'{k} scored {v} in the dice.')
-
+rank = sorted(game.items(), key=itemgetter(1), reverse=True)
 print(game)
+print(rank)
