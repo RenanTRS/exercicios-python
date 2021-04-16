2ec340dd6fdef9ee637028b8e0c27dcb82703a35
RenanTRS <renantrsouza@gmail.com>
2021-04-16 11:43:42 -0400

    inputs included

--- ex090.py
+++ ex090.py
@@ -1,3 +1,7 @@
 #Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da 
 #estrutura na tela.
 
+student = {}
+student['nome'] = str(input("Student's name: ")).strip().title().split()[0]
+student['average'] = float(input("Student's average: "))
+print(student)
