7fe64cdccff064dbbd064ff609b56e706875c3ce
RenanTRS <renantrsouza@gmail.com>
2021-04-19 12:43:30 -0400

    Name included

--- ex092.py
+++ ex092.py
@@ -2,3 +2,8 @@
 #CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, 
 #com quantos anos a pessoa vai se aposentar.
 
+#variables
+register = {}
+
+name = str(input('Name: ')).strip().title().split()[0]
+print(name)
