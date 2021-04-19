4b7f0ba12221957505d6671fc6919d701edb0cd5
RenanTRS <renantrsouza@gmail.com>
2021-04-19 13:00:33 -0400

    age and currentYear included

--- ex092.py
+++ ex092.py
@@ -2,13 +2,19 @@
 #CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, 
 #com quantos anos a pessoa vai se aposentar.
 
+#library
+from datetime import date
 #variables
 register = {}
+currentYear = date.today().year
 
 name = str(input('Name: ')).strip().title().split()[0]
 year = int(input('Year of Birth: '))
+age = currentYear - year
 workCard = int(input('Work permit: '))
+
 print(name)
 print(year)
 print(workCard)
+print(age)
 
