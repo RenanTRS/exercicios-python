95808c2cfeccd5c205c5d6e8402d7756cfec6b74
RenanTRS <renantrsouza@gmail.com>
2021-04-26 13:36:34 -0400

    function averageAge include

--- ex094.py
+++ ex094.py
@@ -4,15 +4,19 @@
 #B) A média de idade
 #C) Uma lista com as mulheres
 #D) Uma lista de pessoas com idade acima da média
+def averageAge(a, b):
+    average = a / b
+    return average
 
 person = {}
 people = []
+count = age = 0
 
 #while logic
 while True:
+    #inputs
     person['name'] = str(input('Name: ')).strip().title().split()[0]
     person['sex'] = str(input('Sex [M/F]: ')).strip().upper().split()[0]
-    
     #if logic
     if person['sex'] != 'M' and person['sex'] != 'F':
         #while logic
@@ -20,13 +24,17 @@ while True:
             person['sex'] = str(input('Error, just input M or F: ')).strip().upper().split()[0]
             if person['sex'] == 'M' or person['sex'] == 'F':
                 break
-
     person['age'] = int(input('Age: '))
-    people.append(person.copy())
+    
+    age += person['age']
+    count += 1
+    people.append(person.copy()) #copy to list
     
     #flag
     opt = str(input('Do you want to continue [Y/N]:')).strip().upper().split()[0]
     if 'N' in opt:
         break
 
+#exit    
 print(people)
+print(f'{averageAge(age, count):.1f}')
