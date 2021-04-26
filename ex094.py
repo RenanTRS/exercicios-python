d9ceb9d0cd3a6447ed33e0869f65b7fa60b940c2
RenanTRS <renantrsouza@gmail.com>
2021-04-26 13:21:29 -0400

    logic included: while, if and flag

--- ex094.py
+++ ex094.py
@@ -5,3 +5,28 @@
 #C) Uma lista com as mulheres
 #D) Uma lista de pessoas com idade acima da média
 
+person = {}
+people = []
+
+#while logic
+while True:
+    person['name'] = str(input('Name: ')).strip().title().split()[0]
+    person['sex'] = str(input('Sex [M/F]: ')).strip().upper().split()[0]
+    
+    #if logic
+    if person['sex'] != 'M' and person['sex'] != 'F':
+        #while logic
+        while True:
+            person['sex'] = str(input('Error, just input M or F: ')).strip().upper().split()[0]
+            if person['sex'] == 'M' or person['sex'] == 'F':
+                break
+
+    person['age'] = int(input('Age: '))
+    people.append(person.copy())
+    
+    #flag
+    opt = str(input('Do you want to continue [Y/N]:')).strip().upper().split()[0]
+    if 'N' in opt:
+        break
+
+print(people)
