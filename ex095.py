64da694b7237717f51d3a7d8a38e6970d95ca4f9
RenanTRS <renantrsouza@gmail.com>
2021-04-27 16:16:13 -0400

    while logic, if and flag

--- ex095.py
+++ ex095.py
@@ -8,26 +8,34 @@ def trace():
     print('-'*30)
 def breakLine():
     print('')
-    
+
 #Inputs----------------------
 player = {}
 players = []
 goals = []
 totalGoals = 0
 
-player['name'] = str(input('Name: ')).strip().title().split()[0]
-qtd = int(input(f'How many matches {player["name"]} played: '))
+#while logic-----------------
+while True:
+    player['name'] = str(input('Name: ')).strip().title().split()[0]
+    qtd = int(input(f'How many matches {player["name"]} played: '))
 
-#for logic-------------------
-for c in range(0, qtd):
-    #print(c)
-    goals.append(int(input(f'How many goals in game {c+1}: ')))
-    totalGoals += goals[c]
+    #for logic-------------------
+    for c in range(0, qtd):
+        #print(c)
+        goals.append(int(input(f'How many goals in game {c+1}: ')))
+        totalGoals += goals[c]
 
-#----------------------------
-player['goals'] = goals[:]
-player['total'] = totalGoals
-players.append(player.copy())
+    #----------------------------
+    player['goals'] = goals[:]
+    player['total'] = totalGoals
+    players.append(player.copy())
+    
+    #flag------------------------
+    opt = str(input('Do you want to continue [Y/N]: ')).strip().upper().split()[0]
+    #if logic
+    if 'N' in opt:
+        break
 
 #exit------------------------
 print(players)
