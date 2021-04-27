44797eb0b30168151b1bff7f4c01b627e2a6da5d
RenanTRS <renantrsouza@gmail.com>
2021-04-27 13:29:20 -0400

    For logic included

--- ex095.py
+++ ex095.py
@@ -5,8 +5,19 @@
 def equals():
     print('=='*30)
 
-#Imputs----------------------
+#Inputs----------------------
 player = {}
+goals = []
+totalGoals = 0
 
 player['name'] = str(input('Name: ')).strip().title().split()[0]
 qtd = int(input(f'How many matches {player["name"]} played: '))
+
+#for logic-------------------
+for c in range(0 < qtd):
+    goals.append(int(input(f'How many goals in game {c+1}: ')))
+    totalGoals += goals[c]
+
+#----------------------------
+player['goals'] = goals[:]
+player['total'] = totalGoals
