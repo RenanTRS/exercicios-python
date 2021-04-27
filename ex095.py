bcfdf24271b32bf56ba96c8c7878cb596c995b3c
RenanTRS <renantrsouza@gmail.com>
2021-04-27 13:24:32 -0400

    Inputs included

--- ex095.py
+++ ex095.py
@@ -1,3 +1,12 @@
 #Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do 
 #aproveitamento de cada jogador.
 
+#Functions-------------------
+def equals():
+    print('=='*30)
+
+#Imputs----------------------
+player = {}
+
+player['name'] = str(input('Name: ')).strip().title().split()[0]
+qtd = int(input(f'How many matches {player["name"]} played: '))
