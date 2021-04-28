1c1eccf97d5f53e26ded34b115e726e740917c4c
RenanTRS <renantrsouza@gmail.com>
2021-04-28 13:35:29 -0400

    Completed

--- ex096.py
+++ ex096.py
@@ -1,3 +1,12 @@
 #Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e 
 #mostre a área do terreno.
 
+def area(w, l):
+    landControl = w * l
+    print(f'The area of a {w} x {l} plot is {landControl}m²')
+
+print('Land Control')
+w = float(input('Width: '))
+l = float(input('Length: '))
+
+area(w, l)
