e465a60f2cdd6337257582c8e8638e6008cc2ee6
RenanTRS <renantrsouza@gmail.com>
2021-04-30 12:18:22 -0400

    Completed, used .center() method.

--- ex097.py
+++ ex097.py
@@ -1,3 +1,12 @@
 #Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com 
 #tamanho adaptável.
 
+def showMe(txt):
+    width = len(txt)+4
+    print('~'*width)
+    print(f'{txt.center(width)}')
+    print('~'*width)
+
+showMe('Renan')
+showMe('Curso de Python no YouTube')
+showMe('CeV')
