7c11ccabd28b6bed0d83c5bb57e970247d74d36b
RenanTRS <renantrsouza@gmail.com>
2021-05-03 14:04:46 -0400

    Completed

--- ex099.py
+++ ex099.py
@@ -1,3 +1,28 @@
 #Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que 
 #analisar todos os valores e dizer qual deles é o maior.
 
+#functions
+def breakLine():
+    print('-='*20)
+def maior(*num):
+    count = bigger = 0
+    for c in num:
+        print(c, end=' ')
+        if count == 0:
+            bigger = c
+            count += 1
+        elif bigger < c:
+            bigger = c
+    print(f'Foram informados {len(num)} ao todo.')
+    print('O maior valor informado foi: ',bigger)
+    breakLine()
+
+
+    #breakLine()
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
