ca376b9005c3ca0a45f0b7b5139c7e2b1a629524
Renan Souza <renantrsouza@gmail.com>
2021-05-07 12:37:53 -0400

    Completed

--- ex105.py
+++ ex105.py
@@ -6,3 +6,31 @@
 #A média da turma
 #A situação (opcional)
 
+def notas(*n, sit=False):
+    cont = media = 0
+    for c in n:
+        if cont == 0:
+            maior = c
+            menor = c
+            cont += 1
+        
+        if c > maior:
+            maior = c
+        
+        if c < menor:
+            menor = c
+        
+        media += c
+    media /= 4
+    aluno = {'maior': maior, 'menor': menor, 'media': media}
+    if sit:
+        if media <= 5:
+            aluno['situacao'] = 'Ruim'
+        elif media > 5:
+            aluno['situacao'] = 'Boa'
+
+    return aluno
+
+
+resp = notas(5.5, 9.5, 10, 6.5, sit=True)
+print(resp)
