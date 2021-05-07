ff51c23ce352d25fe82e1afa66cd096a017b93ad
Renan Souza <renantrsouza@gmail.com>
2021-05-07 11:42:42 -0400

    Completed

--- ex103.py
+++ ex103.py
@@ -1,3 +1,13 @@
 #Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols 
 #ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 
+
+def ficha(nome, qtdGols):
+    if nome == '':
+        nome = '<desconhecido>'
+    return f'O jogador {nome} fez {qtdGols} no campeonato.'
+
+nome = str(input('Nome do jogador: ')).strip().title().split()[0]
+qtdOfGoals = int(input('Quantidade de gols: '))
+
+print(ficha(nome, qtdOfGoals))
