a65d56e3f8d6058e58b154fc55bbbbfb440b8534
Renan Souza <renantrsouza@gmail.com>
2021-05-14 12:01:03 -0400

    add import moeda

--- ex107.py
+++ ex107.py
@@ -1,3 +1,9 @@
 #Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um 
 #programa que importe esse módulo e use algumas dessas funções.
 
+import moeda
+
+num = float(input('Digite o preço R$'))
+moeda.metade(num)
+moeda.dobro(num)
+moeda.aumentar(num)
