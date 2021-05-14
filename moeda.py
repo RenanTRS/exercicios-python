1aae0b0f50f6830398d6033d4e4684bcd260247a
Renan Souza <renantrsouza@gmail.com>
2021-05-14 12:01:34 -0400

    add funções

---
+++ moeda.py
@@ -0,0 +1,12 @@
+def aumentar(num):
+    valor = (num * 10) / 100
+    print(f'Aumentando 10% temos R${valor}')
+
+def diminuir(num):
+    return num
+
+def dobro(num):
+    print(f'O dobro de {num} é {num*2}')
+
+def metade(num):
+    print(f'A metade de {num} é {num/2}')
