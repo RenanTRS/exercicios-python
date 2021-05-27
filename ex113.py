3cca776f33a86bbd4b02473b2c41ee3b3ddb70f8
Renan Souza <renantrsouza@gmail.com>
2021-05-26 21:53:11 -0400

    Completed

--- ex113.py
+++ ex113.py
@@ -2,3 +2,32 @@
 #Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 
 
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print("\033[1;31mErro, por favor digite novamente\033[m")
+        except KeyboardInterrupt:
+            print("\033[1;31mEntrada de dados interrompida pelo usuário\033[m")
+            return 0
+        else:
+            return n
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print("\033[1;31mErro, por favor digite novamente\033[m")
+        except (KeyboardInterrupt):
+            print("\033[1;31mEntrada de dados interrompida pelo usuário\033[m")
+            return 0
+        else:
+            return n
+
+numInt = leiaInt("Digite um número inteiro: ")
+print(f"O valor digitado foi {numInt}")
+
+numFloat = leiaFloat("Digite um número float: ")
+print(f"O valor digitado foi {numFloat}")
