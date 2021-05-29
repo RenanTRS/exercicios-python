b641fee606547fc9e1ded0659e4a75df571d511b
Renan Souza <renantrsouza@gmail.com>
2021-05-28 21:19:58 -0400

    completed

--- ex114.py
+++ ex114.py
@@ -1,3 +1,13 @@
 #Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
 
 
+import urllib.request #Biblioteca que checa o site
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br/')
+except urllib.error.URLError: #urllib.error.URLError é o nome do erro
+#    Caso dê erro
+    print('O site não está disponível.')
+else:
+#   Caso não dê erro
+    print('O site está disponível.')
