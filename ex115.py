757e53103ab9a01a3e5e4b7780a471557977cb0d
Renan Souza <renantrsouza@gmail.com>
2021-06-23 14:27:15 -0400

    add flag

--- ex115.py
+++ ex115.py
@@ -1,2 +1,19 @@
 #Vamos criar um menu em Python, usando modularização.
 
+def menu(msg):
+    print('-'*30)
+    print(f'{msg:^30}')
+    print('-'*30)
+
+while True:
+    menu('Menu Principal')
+    print(f'\033[1;33m1 -\033[1;34m Ver pessoas cadastradas\033[m')
+    print(f'\033[1;33m2 -\033[1;34m Cadastrar nova pessoa\033[m')
+    print(f'\033[1;33m3 -\033[1;34m Sair do Sistema\033[m')
+
+    opc = int(input('Sua Opção: '))
+    if opc == 3: #flag
+        print(f'Saindo do sistema')
+        break
+    else:
+        print(f'{opc}')
