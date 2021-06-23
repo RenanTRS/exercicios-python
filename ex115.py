aa5a8661cb2d52f1517f48034d9d57367618d752
Renan Souza <renantrsouza@gmail.com>
2021-06-23 14:35:13 -0400

    add outras opções

--- ex115.py
+++ ex115.py
@@ -1,5 +1,7 @@
 #Vamos criar um menu em Python, usando modularização.
 
+from time import sleep
+
 def menu(msg):
     print('-'*30)
     print(f'{msg:^30}')
@@ -11,9 +13,19 @@ while True:
     print(f'\033[1;33m2 -\033[1;34m Cadastrar nova pessoa\033[m')
     print(f'\033[1;33m3 -\033[1;34m Sair do Sistema\033[m')
 
-    opc = int(input('Sua Opção: '))
-    if opc == 3: #flag
-        print(f'Saindo do sistema')
-        break
+    try:
+        opc = int(input('\033[1;32mSua Opção: \033[m'))
+    except (ValueError):
+        print(f'\033[1;31mErro: por favor, digite um número inteiro válido.\033[m')
+    
     else:
-        print(f'{opc}')
+        if opc == 3: #flag
+            print(f'Saindo do sistema')
+            break
+        else:
+            if opc == 1:
+                menu('Opção 1')
+                sleep(1)
+            elif opc == 2:
+                menu('Opção 2')
+                sleep(1)
